b12b6f7a423159bb4fc15f4f52bb5684eae62bcd
Priyanka Mehta <primehta17@gmail.com>
2025-04-21 13:22:28 +0530

    some changes

--- five.js
+++ five.js
@@ -1,17 +1,21 @@
 // write a function to swap the case of each character in a string
 
 function swapcase(str){
-    let newStr="";
- for(let i=0;i<str.length;i++){
-    let code= str.charCodeAt(i);
-    if(code >= 65 && code <= 90){
-        newStr += String.fromCharCode(code+32);
-    }else if(code >= 97 && code <= 116){
-        newStr += String.fromCharCode(code-32);
+   let newStr="";
+   for(let i=0;i<str.length;i++){
+    let code = str.charCodeAt(i);
+        // Uppercase A-Z: 65 to 90
+    if(code >= 65 && code <=90){
+        newStr += String.fromCharCode(code+32);// convert to lowercase
+          // Lowercase a-z: 97 to 122
+    }else if(code >=97 && code <=122){
+        newStr += String.fromCharCode(code-32);// convert to uppercase
     }else{
+         // Non-alphabetic characters stay the same
         newStr += str[i];
     }
- }
- console.log(newStr);
+   
+   }
+   return newStr;
 }
-swapcase("Hello World!");
+console.log(swapcase("Hello World!"));
