91a5e947a383841fbf486ec19f53465e7df0e36d
Priyanka Mehta <primehta17@gmail.com>
2025-04-21 12:09:37 +0530

    even number from 1 to n

---
+++ four.js
@@ -0,0 +1,13 @@
+// write a function to add even numbers from 1 to navigator(inclusive)
+
+function addEvenNumbers(n) {
+    let sum=0;
+    for(let i=1;i<=n;i++){
+        
+        if(i%2==0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
+console.log(addEvenNumbers(10));
